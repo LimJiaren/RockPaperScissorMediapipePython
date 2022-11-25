29d8890c2f07ef062c51960f1fc8555201f617ed
Lim Justin <justinlim0505@gmail.com>
2022-11-25 19:44:39 +0800

    add sound

--- code/03_game_rps.py
+++ code/03_game_rps.py
@@ -8,6 +8,7 @@
 import cv2
 import random
 import time
+from playsound import playsound
 
 from threading import Thread
 from utils_display import DisplayHand
@@ -25,6 +26,7 @@ rps_display = None
 nowGesture = None
 winState = None
 x_offset=y_offset=0
+soundeffectFlag = 1
 
 def rps():
     global count_down, start_count_down
@@ -33,7 +35,9 @@ def rps():
             count_down = 0
         time.sleep(1)
         count_down -= 1
-    
+
+def playMusic(music):
+    playsound(music)
 
 # Load mediapipe hand class
 pipe = MediaPipeHand(static_image_mode=False, max_num_hands=1)
@@ -47,9 +51,9 @@ cap = cv2.VideoCapture(0) # By default webcam is index 0
 # Load gesture recognition class
 gest = GestureRecognition(mode='eval')
 
-rock_img = cv2.imread("rock.png")
-paper_img = cv2.imread("paper.png")
-scissor_img = cv2.imread("scissor.png")
+rock_img = cv2.imread("./img/rock.png")
+paper_img = cv2.imread("./img/paper.png")
+scissor_img = cv2.imread("./img/scissor.png")
 
 while cap.isOpened():
     ret, img = cap.read()
@@ -93,28 +97,55 @@ while cap.isOpened():
             list1 = ["rock", "paper", "scissor"]
             rps_result = random.choice(list1)
         if rps_result in ["rock", "paper", "scissor"]:
+
             if rps_result == "rock":
                 rps_display = "rock"
                 if nowGesture == "rock":
                     winState = "TIE"
                 if nowGesture == "paper":
                     winState = "WIN"
+                    if soundeffectFlag == 2:
+                        soundeffect = Thread(target=playMusic, args={"./sound/win.mp3"})
+                        soundeffect.start()
+                        soundeffectFlag = 0 
                 if nowGesture == "scissor":
                     winState = "LOSE"
+                    if soundeffectFlag == 2:
+                        soundeffect = Thread(target=playMusic, args={"./sound/lose.mp3"})
+                        soundeffect.start()
+                        soundeffectFlag = 0 
+
             elif rps_result == "paper":
                 rps_display = "paper"
                 if nowGesture == "rock":
                     winState = "LOSE"
+                    if soundeffectFlag == 2:
+                        soundeffect = Thread(target=playMusic, args={"./sound/lose.mp3"})
+                        soundeffect.start()
+                        soundeffectFlag = 0 
                 if nowGesture == "paper":
                     winState = "TIE"
                 if nowGesture == "scissor":
                     winState = "WIN"
+                    if soundeffectFlag == 2:
+                        soundeffect = Thread(target=playMusic, args={"./sound/win.mp3"})
+                        soundeffect.start()
+                        soundeffectFlag = 0 
+
             else:
                 rps_display = "scissor"
                 if nowGesture == "rock":
                     winState = "WIN"
+                    if soundeffectFlag == 2:
+                        soundeffect = Thread(target=playMusic, args={"./sound/win.mp3"})
+                        soundeffect.start()
+                        soundeffectFlag = 0 
                 if nowGesture == "paper":
                     winState = "LOSE"
+                    if soundeffectFlag == 2:
+                        soundeffect = Thread(target=playMusic, args={"./sound/lose.mp3"})
+                        soundeffect.start()
+                        soundeffectFlag = 0 
                 if nowGesture == "scissor":
                     winState = "TIE"
             resultDisplay = rps_result
@@ -122,17 +153,20 @@ while cap.isOpened():
             rps_result = None
             count_down = 4
             start_count_down = False
+            soundeffectFlag = 1
             start_game = 0
             check_win = 0
 
     
-
     # cv2.putText(img, (f"BOT:{resultDisplay}"), (400, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
     cv2.putText(img, (f"YOU {winState}"), (400, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-    print(f"LOGS: checkwin:{check_win} , start_game:{start_game}, count_down:{count_down}, start_count_down:{start_count_down}")
     img.flags.writeable = True
 
-    if count_down != 0:
+    if count_down != 0 and check_win == 1:
+        if soundeffectFlag == 1:
+            soundeffect = Thread(target=playMusic, args={"./sound/random.mp3"})
+            soundeffect.start()
+            soundeffectFlag = 2 
         temp = random.randint(1, 3)
         if temp == 1: 
             img[y_offset:y_offset+rock_img.shape[0], x_offset:x_offset+rock_img.shape[1]] = rock_img
@@ -148,7 +182,10 @@ while cap.isOpened():
     if rps_display == "scissor":
         img[y_offset:y_offset+scissor_img.shape[0], x_offset:x_offset+scissor_img.shape[1]] = scissor_img
     # Display keypoint and result of rock paper scissor game
+    
     cv2.imshow('Game: Rock Paper Scissor', disp.draw_game_rps(img.copy(), param))
+
+    print(f"LOGS: checkwin:{check_win} , start_game:{start_game}, count_down:{count_down}, start_count_down:{start_count_down}, soundEffectFlag:{soundeffectFlag}")
 
     key = cv2.waitKey(1)
     if key==27:
